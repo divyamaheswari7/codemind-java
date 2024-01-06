a08f680980dcc149ab663318b94af1cc32f3bc73
PALIKALA DIVYA MAHESWARI <119839678+divyamaheswari7@users.noreply.github.com>
2024-01-06 11:50:07 +0530

    import java.util.*; public class Test{     public static void main(String[] args)     {         Scanner sc=new Scanner(System.in);         int s=sc.nextInt();         double res=s/3.6;         System.out.printf("%.2f",res);     } }

---
+++ kmph_to_mps.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class Test{
+    public static void main(String[] args)
+    {
+        Scanner sc=new Scanner(System.in);
+        int s=sc.nextInt();
+        double res=s/3.6;
+        System.out.printf("%.2f",res);
+    }
+}
