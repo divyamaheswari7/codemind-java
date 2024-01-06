03259255813ee66494bd0dbed26c58bd463873c2
PALIKALA DIVYA MAHESWARI <119839678+divyamaheswari7@users.noreply.github.com>
2024-01-06 21:33:06 +0530

    import java.util.*; public class Divya{     public static void main(String[] args)     {         Scanner sc= new Scanner(System.in);         int avg=sc.nextInt();         int b1=sc.nextInt();         int b2=sc.nextInt();         int res=(3*avg)-b1-b2;         System.out.print(res);     } }

---
+++ Average_weight.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class Divya{
+    public static void main(String[] args)
+    {
+        Scanner sc= new Scanner(System.in);
+        int avg=sc.nextInt();
+        int b1=sc.nextInt();
+        int b2=sc.nextInt();
+        int res=(3*avg)-b1-b2;
+        System.out.print(res);
+    }
+}
