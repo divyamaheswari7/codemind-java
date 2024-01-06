618c3c1110264bc6735e131048485f992f5a5a58
PALIKALA DIVYA MAHESWARI <119839678+divyamaheswari7@users.noreply.github.com>
2024-01-06 11:31:07 +0530

    import java.util.*; public class Divya{     public static void main(String[] args)     {         Scanner s=new Scanner(System.in);         int x=s.nextInt();         int y=s.nextInt();         int a=2*x-y;         System.out.print(a);     } }

---
+++ Find_Second_Number.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class Divya{
+    public static void main(String[] args)
+    {
+        Scanner s=new Scanner(System.in);
+        int x=s.nextInt();
+        int y=s.nextInt();
+        int a=2*x-y;
+        System.out.print(a);
+    }
+}
