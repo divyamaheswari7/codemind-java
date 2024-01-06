63ef969c8b359649d6d08cc0290efb7d55867996
PALIKALA DIVYA MAHESWARI <119839678+divyamaheswari7@users.noreply.github.com>
2024-01-06 21:45:04 +0530

    import java.util.*; import java.lang.Math; public class Divya{     public static void main(String[] args)     {         Scanner sc=new Scanner(System.in);         double a=sc.nextInt();         double b=sc.nextInt();         double c=sc.nextInt();         double s=(a+b+c)/2.0d;         double res=Math.sqrt(s*(s-a)*(s-b)*(s-c));         System.out.printf("%.2f",res);     } }

---
+++ Area_of_the_Triangle.java
@@ -0,0 +1,14 @@
+import java.util.*;
+import java.lang.Math;
+public class Divya{
+    public static void main(String[] args)
+    {
+        Scanner sc=new Scanner(System.in);
+        double a=sc.nextInt();
+        double b=sc.nextInt();
+        double c=sc.nextInt();
+        double s=(a+b+c)/2.0d;
+        double res=Math.sqrt(s*(s-a)*(s-b)*(s-c));
+        System.out.printf("%.2f",res);
+    }
+}
