079b9dcc531eb07d7494c63e4e277ec5dc41a4e4
PALIKALA DIVYA MAHESWARI <119839678+divyamaheswari7@users.noreply.github.com>
2024-01-08 11:32:19 +0530

    import java.util.*; public class Div{     public static void main(String[] args)     {         Scanner sc=new Scanner(System.in);         int a=sc.nextInt();         int b=sc.nextInt();         int r=a*5+b*7;         System.out.print(r);     } }

---
+++ King_Tours_.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class Div{
+    public static void main(String[] args)
+    {
+        Scanner sc=new Scanner(System.in);
+        int a=sc.nextInt();
+        int b=sc.nextInt();
+        int r=a*5+b*7;
+        System.out.print(r);
+    }
+}
