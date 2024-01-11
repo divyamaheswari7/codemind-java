875f2c5fb852360392cfb5dceefae07eb2bf3937
PALIKALA DIVYA MAHESWARI <119839678+divyamaheswari7@users.noreply.github.com>
2024-01-11 11:50:38 +0530

    import java.util.*; public class Div{     public static void main(String[] ards)     {         Scanner sc=new Scanner(System.in);         int a=sc.nextInt();         double r=a*2.54;         System.out.printf("%.2f",r);     } }

---
+++ Inches_to_Centimeters.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class Div{
+    public static void main(String[] ards)
+    {
+        Scanner sc=new Scanner(System.in);
+        int a=sc.nextInt();
+        double r=a*2.54;
+        System.out.printf("%.2f",r);
+    }
+}
