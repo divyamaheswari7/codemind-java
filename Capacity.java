48a316079cedd5bdaee2a829ef2b6667fc7c6d8d
PALIKALA DIVYA MAHESWARI <119839678+divyamaheswari7@users.noreply.github.com>
2024-01-11 09:37:34 +0530

    import java.util.Scanner; public class Capacity {     public static void main(String[] args)     {         Scanner sc = new Scanner(System.in);         int t=sc.nextInt();         int s=sc.nextInt();         int b=sc.nextInt();         int c;         c=(2*t*s*b*512)/1024;         System.out.printf("%d KB",c);     } }

---
+++ Capacity.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+public class Capacity
+{
+    public static void main(String[] args)
+    {
+        Scanner sc = new Scanner(System.in);
+        int t=sc.nextInt();
+        int s=sc.nextInt();
+        int b=sc.nextInt();
+        int c;
+        c=(2*t*s*b*512)/1024;
+        System.out.printf("%d KB",c);
+    }
+}
