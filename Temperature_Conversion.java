73946cdb3e5af85116547b2d929b576042cdac79
PALIKALA DIVYA MAHESWARI <119839678+divyamaheswari7@users.noreply.github.com>
2024-01-20 17:15:26 +0530

    import java.util.Scanner; public class Herons {     public static void main(String[] args)     {         Scanner sc=new Scanner(System.in);         double c=sc.nextDouble();         double fe=32+(c*9/5);         System.out.printf("%.2f",fe);     } }

---
+++ Temperature_Conversion.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+public class Herons
+{
+    public static void main(String[] args)
+    {
+        Scanner sc=new Scanner(System.in);
+        double c=sc.nextDouble();
+        double fe=32+(c*9/5);
+        System.out.printf("%.2f",fe);
+    }
+}
