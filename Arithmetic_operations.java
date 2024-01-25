47a2385dc4ba747892832821640581eb86bf88a7
PALIKALA DIVYA MAHESWARI <119839678+divyamaheswari7@users.noreply.github.com>
2024-01-25 09:39:49 +0530

    import java.util.*; public class Div{     public static void main(String[] ags){         Scanner sc=new Scanner(System.in);         int a=sc.nextInt();         int b=sc.nextInt();         System.out.printf("Sum:%d ",a+b);         System.out.printf("Difference:%d ",a-b);         System.out.printf("Product:%d ",a*b);         System.out.printf("Quotient:%d ",a/b);         System.out.printf("Remainder:%d ",a%b);     } }

---
+++ Arithmetic_operations.java
@@ -0,0 +1,18 @@
+import java.util.*;
+public class Div{
+    public static void main(String[] ags){
+        Scanner sc=new Scanner(System.in);
+        int a=sc.nextInt();
+        int b=sc.nextInt();
+        System.out.printf("Sum:%d
+",a+b);
+        System.out.printf("Difference:%d
+",a-b);
+        System.out.printf("Product:%d
+",a*b);
+        System.out.printf("Quotient:%d
+",a/b);
+        System.out.printf("Remainder:%d
+",a%b);
+    }
+}
