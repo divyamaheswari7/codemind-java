874174e598b9859d7f4a65247adc2796c97b70f1
PALIKALA DIVYA MAHESWARI <119839678+divyamaheswari7@users.noreply.github.com>
2024-02-01 11:03:56 +0530

    import java.util.*; public class Div{     public static void main(String[] args){         Scanner sc=new Scanner(System.in);         int x=sc.nextInt();         int b=x*4;         if(b<=1000)System.out.println("YES");         else System.out.println("NO");     } }

---
+++ Tickets.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class Div{
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        int x=sc.nextInt();
+        int b=x*4;
+        if(b<=1000)System.out.println("YES");
+        else System.out.println("NO");
+    }
+}
