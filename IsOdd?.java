631c5259e9f7e4e04db34197612ff9766ff9ff38
PALIKALA DIVYA MAHESWARI <119839678+divyamaheswari7@users.noreply.github.com>
2024-03-06 11:30:47 +0530

    import java.util.*; public class Div{     public static void main(String[] args){         Scanner sc=new Scanner(System.in);         int a=sc.nextInt();         if(a%2!=0)System.out.println(2);         else System.out.println(1);     } }

---
+++ IsOdd?.java
@@ -0,0 +1,9 @@
+import java.util.*;
+public class Div{
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        int a=sc.nextInt();
+        if(a%2!=0)System.out.println(2);
+        else System.out.println(1);
+    }
+}
